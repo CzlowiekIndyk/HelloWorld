6b40941d908734bc6323aafb2a0848eac6d0e03d
Administrator <Administrator@W-H-CZC249BBD7>
2023-04-24 15:20:21 +0200

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System; // wywołania zewnętrznych bibliotek
+
+namespace HelloWorld //folder w którym będzie znajdował się plik
+{
+
+    class Program  //inicjalizacja obiektu
+    {
+        static void Main(string[] args)  //metoda Main - główne miejce uruchomienia aplikacji
+        {
+            string imie = "Roman";
+            Console.WriteLine("HelloWorld");
+            Console.WriteLine("Nazywam sie Roman Bułkowski");
+        }
+    }
+}
